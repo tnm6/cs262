62a1a208de45051e8e41235f4db15502a8ceda7e
tnm6 <tnm6@students.calvin.edu>
2019-09-25 21:01:25 -0400

    Added some work on tutorial app and initial homework1 stub

---
+++ homework1/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Homework1());
+
+class Homework1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Homework 1',
+      home:Calculator()
+    );
+  }
+}
