b8be02c5ff36c9d725cf356bd1afbc4dd8459951
tnm6 <tnm6@students.calvin.edu>
2019-09-26 23:30:16 -0400

    Added documentation and submission image

--- homework1/lib/main.dart
+++ homework1/lib/main.dart
@@ -1,8 +1,11 @@
+/// Homework1 is a simple calculator app with 
+/// addition, subtraction, multiplication, and division.
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Homework1());
 
-class Homework1 extends StatelessWidget{
+class Homework1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,21 +20,28 @@ class CalculatorApp extends StatefulWidget {
   Calculator createState() => Calculator();
 }
 
+/// Calculator creates the layout and functions of the app
 class Calculator extends State<CalculatorApp> {
+  // Controllers for user input
   final _controllerNum1 = TextEditingController();
   final _controllerNum2 = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
 
+  // Resultant value to be displayed after operations
   String _resultText = "";
 
+  /// Methods for addition, subtraction, multiplication, and division
+  /// See _sum() for explanation
   void _sum() {
     if (_formKey.currentState.validate()) {
+      // Variables read from controllers in TextFormFields
       int num1 = int.parse(_controllerNum1.text);
       int num2 = int.parse(_controllerNum2.text);
-      int result = num1 + num2;
+      int result = num1 + num2; // sum the values
 
       setState(() {
-        _resultText = "$num1 + $num2 = $result";
+        _resultText = "$num1 + $num2 = $result";  // print them with operator
       });
     }
   }
@@ -69,6 +79,8 @@ class Calculator extends State<CalculatorApp> {
     }
   }
 
+  /// Layout of the Calculator,
+  /// including two value fields and a row of basic operators
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,9 +92,10 @@ class Calculator extends State<CalculatorApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // The two fields for input values
             TextFormField(
               controller: _controllerNum1,
-              validator: (value) {
+              validator: (value) {  // Catches any empty inputs
                 if (value.isEmpty) return "Please enter a number";
               },
               decoration: InputDecoration(hintText: "Enter Value 1"),
@@ -90,18 +103,13 @@ class Calculator extends State<CalculatorApp> {
             ),
             TextFormField(
               controller: _controllerNum2,
-              validator: (value) {
+              validator: (value) {  // Catches any empty inputs
                 if (value.isEmpty) return "Please enter a number";
               },
               decoration: InputDecoration(hintText: "Enter Value 2"),
               keyboardType: TextInputType.number,
             ),
-
-            Text(
-              _resultText,
-              style: TextStyle(fontSize: 20.0),
-            ),
-
+            // Row of buttons for operators (+, -, *, /)
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -122,6 +130,11 @@ class Calculator extends State<CalculatorApp> {
                   child: Text('/'),
                 ),
               ],
+            ),
+            // Text field for resulting value
+            Text(
+              _resultText,
+              style: TextStyle(fontSize: 20.0),
             ),
           ]
         ),
