703ea87c632fad110794da1b967df7eac636de13
tnm6 <tnm6@students.calvin.edu>
2019-10-09 18:07:39 -0400

    Row with textfield ready for send button

--- lab6/lib/main.dart
+++ lab6/lib/main.dart
@@ -18,10 +18,36 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: new InputDecoration.collapsed(
+                hintText: "Send a message"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat (Lab 6)")),
+
+      body: _buildTextComposer(),
     );
   }
 }
