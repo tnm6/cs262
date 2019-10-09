105edd51de71c2c45213481af22d9e827aa9e041
tnm6 <tnm6@students.calvin.edu>
2019-10-09 18:29:21 -0400

    ChatMessage class

--- lab6/lib/main.dart
+++ lab6/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new FriendlychatApp());
 
+const String _name = "Nathan";
+
 class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,25 +27,28 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: new TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration: new InputDecoration.collapsed(
-                hintText: "Send a message"),
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                  hintText: "Send a message"),
+              ),
             ),
-          ),
-          new Container(
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: new IconButton(
-              icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text)),
-          ),
-        ],
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -54,6 +59,37 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(title: new Text("Friendlychat (Lab 6)")),
 
       body: _buildTextComposer(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
